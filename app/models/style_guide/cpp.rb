243fed76eefc6b14388b78dc3582dda837671103
Ryo Miyajima <sergeant.wizard@gmail.com>
2014-10-07 11:50:45 +0900

    fix bugs found using byebug

--- app/models/style_guide/cpp.rb
+++ app/models/style_guide/cpp.rb
@@ -1,14 +1,21 @@
+require 'kconv'
+
 module StyleGuide
   class Cpp < Base
-    def violations_in_file(file)
-      # TODO: cpplint in a gem is prefered
-      cpplint_path = Rails.root.join('bin','cpplint-0.0.3','cpplint','cpplint.py')
-      cpplint_output = exec "python #{cpplint_path} #{file}"
-      # cpplint does not print violations in the last two lines
-      cpplint_output.to_a[0..-2].each do |line|
-        (file, line_number, [reason]) = line.split(':')
-        Violation.new(file, line_number, reason.join(':'))
-      end
+  def violations(file)
+    # TODO: cpplint in a gem is prefered
+    cpplint_path = Rails.root.join('bin','cpplint.py')
+    File.open('tmp.cpp', 'w') { |tmpfile| tmpfile.write file.content.toutf8 }
+    cpplint_output = `python #{cpplint_path} tmp.cpp 2>&1`.split("\n")
+    # cpplint does not print violations in the last two lines
+    cpplint_output = cpplint_output[0..-3]
+    cpplint_output.map do |line|
+      (_, line_number, reason) = line.split(':',3)
+      # mock model of RuboCop::Cop::Team#inspect_file
+      OpenStruct.new(
+        line: line_number.to_i,
+        message: reason
+      )
     end
   end
 end
